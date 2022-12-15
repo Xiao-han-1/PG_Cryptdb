746dd9d2e0871dd590ac7903af78df4546d7208f
Xiao-han-1 <xiao981217@163.com>
2022-12-15 06:48:37 +0000

    save-12-compute

--- Crypto-ckks/ckks.cpp
+++ Crypto-ckks/ckks.cpp
@@ -1,146 +1,286 @@
-#include "seal/ckks.h"
-#include <algorithm>
-#include <chrono>
-#include <cstddef>
-#include <fstream>
+#include<ckks.hh>
+#include<string>
+#include<sstream>
+#include "openfhe.h"
 #include <iomanip>
-#include <iostream>
-#include <limits>
-#include <memory>
-#include <mutex>
-#include <numeric>
-#include <random>
-#include <sstream>
-#include <vector>
-#include <string>
-#include <thread>
-#include <vector>
-#include <seal/seal.h>
-#include <seal/ciphertext.h>
+#include <tuple>
+#include <unistd.h>
+// header files needed for serialization
+#include "ciphertext-ser.h"
+#include "cryptocontext-ser.h"
+#include "key/key-ser.h"
+#include "scheme/ckksrns/ckksrns-ser.h"
+#include "key/keypair.h"
+using namespace lbcrypto;
 using namespace std;
-using namespace seal;
-#include <vector>
-#define N 3
-//本例的目的是计算x，y，z的乘积
-
-
-//构建参数容器 parms
-/*CKKS有三个重要参数：
-1.poly_module_degree(多项式模数)
-2.coeff_modulus（参数模数）
-3.scale（规模）
-下一小节会详细解释*/
-
-//构建各模块
-//首先构建keygenerator，生成公钥、私钥和重线性化密钥
-
-double scale;
-auto context,public_key,secret_key,relin_keys;
-void  build_parms()
-{
-	EncryptionParameters parms(scheme_type::CKKS);
-/*CKKS有三个重要参数：
-1.poly_module_degree(多项式模数)
-2.coeff_modulus（参数模数）
-3.scale（规模）
-下一小节会详细解释*/
-
-size_t poly_modulus_degree = 8192;
-parms.set_poly_modulus_degree(poly_modulus_degree);
-parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, { 60, 40, 40, 60 }));
-//选用2^40进行编码
-
-//用参数生成CKKS框架context
-context = SEALContext::Create(parms);
-double scale = pow(2.0, 40); 
-	KeyGenerator keygen(context);
-	 public_key = keygen.public_key();
-     secret_key = keygen.secret_key();
-	 relin_keys = keygen.relin_keys();
-
-//构建编码器，加密模块、运算器和解密模块
-//注意加密需要公钥pk；解密需要私钥sk；编码器需要scale
-	Encryptor encryptor(context, public_key);
-	Evaluator evaluator(context);
-	Decryptor decryptor(context, secret_key);
-
-	CKKSEncoder encoder(context);
+/////////////////////////////////////////////////////////////////
+// NOTE:
+// If running locally, you may want to replace the "hardcoded" DATAFOLDER with
+// the DATAFOLDER location below which gets the current working directory
+/////////////////////////////////////////////////////////////////
+// char buff[1024];
+// std::string DATAFOLDER = std::string(getcwd(buff, 1024));
+inline bool exists_test (const std::string& name) {
+    ifstream f(name.c_str());
+    return f.good();
 }
-Ciphertext 
-Encrypt_Ckks(double  x)
+// Save-Load locations for keys
+const std::string DATAFOLDER = "/home/demoData";
+std::string ccLocation       = "/cryptocontext.txt";
+std::string pubKeyLocation   = "/key_pub.txt"; 
+std::string secKeyLocation   = "/key_sec.txt";   // Pub key
+std::string multKeyLocation  = "/key_mult.txt";  // relinearization key
+std::string rotKeyLocation   = "/key_rot.txt";   // automorphism / rotation key
+
+// Save-load locations for RAW ciphertexts
+std::string cipherOneLocation = "/ciphertext";
+std::string cipherTwoLocation = "/ciphertext2.txt";
+// Save-load locations for evaluated ciphertexts
+std::string cipherMultLocation   = "/ciphertextMult";
+std::string cipherAddLocation    = "/ciphertextAdd";
+std::string cipherRotLocation    = "/ciphertextRot";
+std::string cipherRotNegLocation = "/ciphertextRotNegLocation.txt";
+std::string clientVectorLocation = "/ciphertextVectorFromClient.txt";
+void FHE_CKKS::Init()
 {
-	
-//对向量x、y、z进行编码
-	Plaintext xp;
-	// yp, zp;
-	encoder.encode(x, scale, &xp);
-	// encoder.encode(y, scale, yp);
-	// encoder.encode(z, scale, zp);
-//对明文xp、yp、zp进行加密
-	Ciphertext xc;
-	// yc, zc;
-	encryptor.encrypt(xp, xc);
-	// encryptor.encrypt(yp, yc);
-	// encryptor.encrypt(zp, zc);
-	return xc;
-}
+    if(exists_test(DATAFOLDER+ccLocation)==false)
+    {
+    CCParams<CryptoContextCKKSRNS> parameters;
+    parameters.SetMultiplicativeDepth(multDepth);
+    parameters.SetScalingModSize(scaleModSize);
+    parameters.SetBatchSize(batchSize);
+    CryptoContext<DCRTPoly> serverCC ;
+    serverCC = GenCryptoContext(parameters);
 
-/*对密文进行计算，要说明的原则是：
-1.加法可以连续运算，但乘法不能连续运算
-2.密文乘法后要进行relinearize操作
-3.执行乘法后要进行rescaling操作
-4.进行运算的密文必需执行过相同次数的rescaling（位于相同level）
-*/
-//基于上述原则进行运算
+    serverCC->Enable(PKE);
+    serverCC->Enable(KEYSWITCH);
+    serverCC->Enable(LEVELEDSHE);
 
-//至此，客户端将pk、CKKS参数发送给服务器，服务器开始运算
-//服务器的视角：先设中间变量
-Ciphertext 
-HOM_MUL(Ciphertext xc,Ciphertext yc,char compute)
+    KeyPair<DCRTPoly> serverKP = serverCC->KeyGen();
+    serverCC->EvalMultKeyGen(serverKP.secretKey);
+    serverCC->EvalRotateKeyGen(serverKP.secretKey, {1, 2, -1, -2});
+     if (!Serial::SerializeToFile( DATAFOLDER + ccLocation, serverCC, SerType::BINARY)) {
+        std::cerr << "Error writing serialization of the crypto context to "
+                     "cryptocontext.txt"
+                  << std::endl;
+        std::exit(1);
+    }
+
+    if (!Serial::SerializeToFile( DATAFOLDER + pubKeyLocation, serverKP.publicKey, SerType::BINARY)) {
+        std::cerr << "Exception writing public key to pubkey.txt" << std::endl;
+        std::exit(1);
+    }
+    if (!Serial::SerializeToFile( DATAFOLDER +secKeyLocation, serverKP.secretKey, SerType::BINARY)) {
+        std::cerr << "Exception writing public key to pubkey.txt" << std::endl;
+        std::exit(1);
+    }
+    std::ofstream multKeyFile( DATAFOLDER + multKeyLocation, std::ios::out | std::ios::binary);
+    if (multKeyFile.is_open()) {
+        if (!serverCC->SerializeEvalMultKey(multKeyFile, SerType::BINARY)) {
+            std::cerr << "Error writing eval mult keys" << std::endl;
+            std::exit(1);
+        }
+        multKeyFile.close();
+    }
+    else {
+        std::cerr << "Error serializing EvalMult keys" << std::endl;
+        std::exit(1);
+    }
+
+    std::ofstream rotationKeyFile( DATAFOLDER + rotKeyLocation, std::ios::out | std::ios::binary);
+    if (rotationKeyFile.is_open()) {
+        if (!serverCC->SerializeEvalAutomorphismKey(rotationKeyFile, SerType::BINARY)) {
+            std::cerr << "Error writing rotation keys" << std::endl;
+            std::exit(1);
+        }
+    }
+    else {
+        std::cerr << "Error serializing Rotation keys" << std::endl;
+        std::exit(1);
+    }
+    }
+    
+ }
+int k_init()
 {
-	Ciphertext temp;
-	Ciphertext result_c;
-	size_t xc_chain=context->get_context_data(xc.parms_id())->chain_index();
-	size_t yc_chain=context->get_context_data(yc.parms_id())->chain_index();
-    if(xc_chain>yc_chain)
-	{
-		size_t d_value=xc_chain-yc_chain;
-		while (d_value--)
-		{
-			Plaintext wt;
-	        encoder.encode(1.0, scale, wt);
-			evaluator.multiply_plain_inplace(yc, wt);
-	        evaluator.rescale_to_next_inplace(yc);
-		}	
-	}
-	else {
-		size_t d_value=yc_chain-xc_chain;
-		while (d_value--)
-		{
-			Plaintext wt;
-	        encoder.encode(1.0, scale, wt);
-			evaluator.multiply_plain_inplace(xc, wt);
-	        evaluator.rescale_to_next_inplace(xc);
-		}	
-
-	}
-	evaluator.multiply(xc,yc,temp);
-	evaluator.relinearize_inplace(temp, relin_keys);
-	evaluator.rescale_to_next_inplace(temp,result_c);
-	return result_c;
-
+ std::string name ="/k.txt";
+//  if(!exists_test( DATAFOLDER + name))
+//  {
+//     ofstream fout( DATAFOLDER + name);
+//     fout<<0;
+//    fout.close();
+//  }
+int k;
+  ifstream readFile( DATAFOLDER + name);
+  readFile >> k;
+  readFile.close();
+  k++;
+   ofstream fout( DATAFOLDER + name);
+    fout<<k;
+   fout.close();
+  return k;
 }
-//计算完毕，服务器把结果发回客户端
-vector<double> 
-Decrypt_Ckks(Ciphertext result_c)
+std::string FHE_CKKS::Encrypt(double c)
 {
+  std::vector<std::complex<double>> vec;
+  vec.push_back(c);
+  CryptoContext<DCRTPoly> serverCC;
+  lbcrypto::CryptoContextFactory<lbcrypto::DCRTPoly>::ReleaseAllContexts();
+    if (!Serial::DeserializeFromFile( DATAFOLDER + ccLocation, serverCC, SerType::BINARY)) {
+        std::cerr << "I cannot read serialized data from: " << DATAFOLDER << "/cryptocontext.txt" << std::endl;
+        std::exit(1);
+    }
 
-	//客户端进行解密和解码
-	Plaintext result_p;
-	decryptor.decrypt(result_c, result_p);
-//注意要解码到一个向量上
-	vector<double> result;
-	encoder.decode(result_p, result);
-	return result;
+    KeyPair<DCRTPoly> serverKP;  // We do NOT have a secret key. The client
+    // should not have access to this
+    PublicKey<DCRTPoly> serverPublicKey;
+    if (!Serial::DeserializeFromFile( DATAFOLDER + pubKeyLocation, serverPublicKey, SerType::BINARY)) {
+        std::cerr << "I cannot read serialized data from: " << DATAFOLDER << "/cryptocontext.txt" << std::endl;
+        std::exit(1);
+    }
+
+    Plaintext serverP1 = serverCC->MakeCKKSPackedPlaintext(vec);
+    auto serverC1 = serverCC->Encrypt(serverPublicKey, serverP1);
+    int k=k_init();
+    std::string num_str=std::to_string(k);
+    std::string store= DATAFOLDER + cipherOneLocation+num_str+".txt";
+      if (!Serial::SerializeToFile(store, serverC1, SerType::BINARY)) {
+        std::cerr << " Error writing ciphertext 1" << std::endl;
+    }   
+    std::ifstream file(store, std::ios::in | std::ios::binary);
+    std::stringstream re;
+    re<<file.rdbuf();
+    // return re.str();
+    // re<<DATAFOLDER <<cipherOneLocation<<num_str<<".txt";
+    std::cout<<re.str().length()<<"\n";
+    return store;
 }
+double FHE_CKKS::Decrypt(std::string c1)
+{
+  
+  CryptoContext<DCRTPoly> serverCC;
+  lbcrypto::CryptoContextFactory<lbcrypto::DCRTPoly>::ReleaseAllContexts();
+    if (!Serial::DeserializeFromFile( DATAFOLDER + ccLocation, serverCC, SerType::BINARY)) {
+        std::cerr << "I cannot read serialized data from: " << DATAFOLDER << "/cryptocontext.txt" << std::endl;
+        std::exit(1);
+    }
+    KeyPair<DCRTPoly> serverKP;  // We do NOT have a secret key. The client
+    // should not have access to this
+    PrivateKey<DCRTPoly> serverSecretKey;
+    if (!Serial::DeserializeFromFile( DATAFOLDER + secKeyLocation, serverSecretKey, SerType::BINARY)) {
+        std::cerr << "I cannot read serialized data from: " << DATAFOLDER << "/cryptocontext.txt" << std::endl;
+        std::exit(1);
+    }
+     Ciphertext<DCRTPoly> EncryptC1;
+     if (!Serial::DeserializeFromFile( c1, EncryptC1, SerType::BINARY)) {
+        std::cerr << "Cannot read serialization from " << c1 << std::endl;
+        std::exit(1);
+    }
+    Plaintext PlaintextC1;
+    serverCC->Decrypt(serverSecretKey,EncryptC1, &PlaintextC1);
+    PlaintextC1->SetLength(32);
+  const std::vector<double> myVec = PlaintextC1->GetRealPackedValue();
+  double er=(double)myVec[0];
+  return er;
+}
+std::string FHE_CKKS::Mul(std::string c1,std::string c2)
+{
+    Ciphertext<DCRTPoly> clientC1;
+    Ciphertext<DCRTPoly> clientC2;
+    CryptoContext<DCRTPoly> serverCC;
+    lbcrypto::CryptoContextFactory<lbcrypto::DCRTPoly>::ReleaseAllContexts();
+    
+    if (!Serial::DeserializeFromFile( DATAFOLDER + ccLocation, serverCC, SerType::BINARY)) {
+        std::cerr << "I cannot read serialized data from: " << DATAFOLDER << "/cryptocontext.txt" << std::endl;
+        std::exit(1);
+    }
+    std::ifstream multKeyIStream( DATAFOLDER + multKeyLocation, std::ios::in | std::ios::binary);
+    if (!multKeyIStream.is_open()) {
+        std::cerr << "Cannot read serialization from " << DATAFOLDER + multKeyLocation << std::endl;
+        std::exit(1);
+    }
+    if (!serverCC->DeserializeEvalMultKey(multKeyIStream, SerType::BINARY)) {
+        std::cerr << "Could not deserialize eval mult key file" << std::endl;
+        std::exit(1);
+    }
+    if (!Serial::DeserializeFromFile( c1, clientC1, SerType::BINARY)) {
+        std::cerr << "Cannot read serialization from " << c1 << std::endl;
+        std::exit(1);
+    }
 
+    if (!Serial::DeserializeFromFile( c2, clientC2, SerType::BINARY)) {
+        std::cerr << "Cannot read serialization from " << c2 << std::endl;
+        std::exit(1);
+    }
+    auto clientCiphertextMult   = serverCC->EvalMult(clientC1, clientC2);
+    int k=k_init();
+    std::string num_str=std::to_string(k);
+     std::string str= DATAFOLDER + cipherMultLocation+num_str+".txt";
+     Serial::SerializeToFile(str, clientCiphertextMult, SerType::BINARY);
+     return str;
+
+}
+std::string FHE_CKKS::Add(std::string c1,std::string c2)
+{
+  Ciphertext<DCRTPoly> clientC1;
+    Ciphertext<DCRTPoly> clientC2;
+    CryptoContext<DCRTPoly> serverCC;
+    lbcrypto::CryptoContextFactory<lbcrypto::DCRTPoly>::ReleaseAllContexts();
+    if (!Serial::DeserializeFromFile( DATAFOLDER + ccLocation, serverCC, SerType::BINARY)) {
+        std::cerr << "I cannot read serialized data from: " << DATAFOLDER << "/cryptocontext.txt" << std::endl;
+        std::exit(1);
+    }
+    if (!Serial::DeserializeFromFile( c1, clientC1, SerType::BINARY)) {
+        std::cerr << "Cannot read serialization from " << DATAFOLDER + cipherOneLocation << std::endl;
+        std::exit(1);
+    }
+
+    if (!Serial::DeserializeFromFile( c2, clientC2, SerType::BINARY)) {
+        std::cerr << "Cannot read serialization from " << DATAFOLDER + cipherTwoLocation << std::endl;
+        std::exit(1);
+    }
+    auto clientCiphertextAdd    = serverCC->EvalAdd(clientC1, clientC2);
+    std::string cipherAddLocation="/cipherAddLocation";
+    int k=k_init();
+    std::string num_str=std::to_string(k);
+     std::string str= DATAFOLDER + cipherAddLocation+num_str+".txt";
+     Serial::SerializeToFile(str, clientCiphertextAdd, SerType::BINARY);
+     return str;
+     
+}
+std::string FHE_CKKS::Sub(std::string c1,std::string c2)
+{
+  Ciphertext<DCRTPoly> clientC1;
+    Ciphertext<DCRTPoly> clientC2;
+    CryptoContext<DCRTPoly> serverCC;
+    lbcrypto::CryptoContextFactory<lbcrypto::DCRTPoly>::ReleaseAllContexts();
+    if (!Serial::DeserializeFromFile( DATAFOLDER + ccLocation, serverCC, SerType::BINARY)) {
+        std::cerr << "I cannot read serialized data from: " << DATAFOLDER << "/cryptocontext.txt" << std::endl;
+        std::exit(1);
+    }
+    if (!Serial::DeserializeFromFile( c1, clientC1, SerType::BINARY)) {
+        std::cerr << "Cannot read serialization from " << DATAFOLDER + cipherOneLocation << std::endl;
+        std::exit(1);
+    }
+
+    if (!Serial::DeserializeFromFile( c2, clientC2, SerType::BINARY)) {
+        std::cerr << "Cannot read serialization from " << DATAFOLDER + cipherTwoLocation << std::endl;
+        std::exit(1);
+    }
+    auto clientCiphertextSub = serverCC->EvalSub(clientC1, clientC2);
+    std::string cipherSubLocation="/cipherSubLocation";
+    int k=k_init();
+    std::string num_str=std::to_string(k);
+     std::string str= DATAFOLDER + cipherSubLocation+num_str+".txt";
+     Serial::SerializeToFile(str, clientCiphertextSub, SerType::BINARY);
+     return str;
+}
+int FHE_CKKS::Compare(std::string c1,std::string c2)
+{
+  
+    std::string EncryptSub=Sub(c1,c2);
+    double res=Decrypt(EncryptSub);
+    if(res<0) return -1;
+    if(res>0) return 1;
+    else return 0;
+}
